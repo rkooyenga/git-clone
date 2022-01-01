38072840949262ff2252a6e34a066a86936e3a37
deadflowers <deadflowers@users.noreply.github.com>
2022-01-01 03:08:35 -0500

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -3,19 +3,25 @@
 # by ray kooyenga @rayktweets
 # created 03-04-2014
 # revised 01-01-2021
-printf "\n Enter the user or org name whose repositories you want to clone: \n"
-read GITORG
-mkdir $GITORG
-cd $GITORG
+myecho() {
+  echo -e "\e[33m${1}\e[0m"
+}
 GITCMD="git clone "
 # alternate clone methods
 # GITCMD="git clone --depth 1 --single-branch --branch master "
 # GITCMD="git clone --mirror "
+myecho 'Enter the user or org name whose repositories you want to clone: '
+read GITORG
 REPOLIST=""
 for PAGE in {1..3}; do
   REPOLIST+=`curl --silent "https://api.github.com/users/${GITORG}/repos?per_page=100&page=${PAGE}" -q|sort|grep "\"clone_url\""|awk -F': "' '{print $2}' | sed -e 's/",//g'`
 done
-printf "\n Cloning repositories of $GITORG... \n"
+myecho ' $REPOLIST '
+myecho ' Cloning the above repositories of $GITORG press Enter to continue or Ctrl C to cancel'
+read
+mkdir $GITORG
+cd $GITORG
 for REPO in $REPOLIST; do
   ${GITCMD}${REPO}
 done
+myecho ' finished cloning '
